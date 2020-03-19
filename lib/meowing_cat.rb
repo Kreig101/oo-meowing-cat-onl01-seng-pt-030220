587aa92ad6bc7cd1d170b40c98544eea5c7a6a0b
Craig Storer <craigstorer@gmail.com>
2020-03-19 04:21:19 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,12 +1,6 @@
    class Cat
-     
-     def name=(name)
-       @name = name
-     end
-     
-     def name
-       @name 
-     end
+
+    attr_accessor :name
      
      def meow
        puts "meow!"
