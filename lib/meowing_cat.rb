7784d113037d5486031b04e5b853c6c331801af3
Craig Storer <craigstorer@gmail.com>
2020-03-19 04:07:14 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,4 +1,15 @@
    class Cat
      
+     def name=(name)
+       @name = name
+     end
+     
+     def name
+       @name 
+     end
+     
+     def meow
+       puts "meow!"
+     end
      
     end
